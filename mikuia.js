8c3b87e790addbba4cecbbccf587b575dca790b3
Léo Maradan <leomaradan@gmail.com>
2017-07-09 14:58:06 +0200

    Add CSV support

--- mikuia.js
+++ mikuia.js
@@ -1,8 +1,41 @@
 var data = [];
 
 function parse() {
+  if($("input[name=optionsDatas]:checked").val() == 'csv') {
+    parseCSV();
+  }
+  else {
+    parseMikuia();
+  }
+
+  $("input[name=optionsDatas]").attr('disabled','disabled');
+
+  $("#options").removeClass("hidden");  
+}
+
+function parseCSV() {
+  data = [];
+  var lines = $("#csvdata textarea").val().split("\n");
+  var item = {};
+  var i, line;  
+
+  for(i in lines) {
+    line = lines[i];
+
+    if(line !== "Username,Subscribe Date,Paying") {
+      item.rank = data.length + 1;
+      item.name = line.split(",")[0];
+      data.push(item);
+      item = {};
+    }
+  }  
+
+  $("#csvdata").addClass("hidden");
+}
+
+function parseMikuia() {
 	data = [];
-	var lines = $("#rawdata").val().split("\n");
+	var lines = $("#data textarea").val().split("\n");
   var item = {};
   var i, line;
   
@@ -22,9 +55,10 @@ function parse() {
       }
     }
   }
-  
+
   $("#data").addClass("hidden");
-  $("#options").removeClass("hidden");
+  
+
 }
 
 function generate() {
@@ -34,9 +68,14 @@ function generate() {
   
   var filtre_method = $("input[name=optionsFiltre]:checked").val();
   var tickets_method = $("input[name=optionsMethode]:checked").val();
+
+  setStore("filtre", filtre_method);
+  setStore("methode", tickets_method);
   
   for(var i in data) {
-    if(filtre_method == "rang") {
+    if(limit == "" || limit == 0) {
+      filter.push(data[i]);
+    } else if(filtre_method == "rang") {
     	if(data[i].rank <= limit) {
       	filter.push(data[i]);
       }
@@ -67,7 +106,41 @@ function Shuffle(o) {
 	return o;
 };
 
-$("#parse").click(parse);
+function setStore(key, data) {
+  if(typeof localStorage != 'undefined') { 
+    localStorage.setItem(key, data);
+  }
+};
+
+function getStore(key) {
+  if(typeof localStorage != 'undefined') { 
+    return localStorage.getItem(key);
+  }
+  return null;
+};
+
+/* 
+  Binding functions
+*/
+
+$("input[name=optionsDatas]").change(function() {
+
+  var source = $("input[name=optionsDatas]:checked").val();
+
+  setStore("source", source);
+
+  if(source == 'csv') {
+    $("#data").addClass("hidden");
+    $("#optionsFiltrePanel").addClass("hidden");
+    $("#csvdata").removeClass("hidden");
+  } else {
+    $("#data").removeClass("hidden");
+    $("#optionsFiltrePanel").removeClass("hidden");
+    $("#csvdata").addClass("hidden");
+  }
+});
+
+$(".buttonParse").click(parse);
 
 $("#tirage").click(function() {
   var tickets = generate();
@@ -88,3 +161,25 @@ $("#generate").click(function() {
   
   $("#randomorg").val(tickets.join("\n"));
 });
+
+/* 
+  Loading functions
+*/
+
+if(getStore("source") == "csv") {
+  $("#dataFromCSV").click();
+} else {
+  $("#dataFromMikuia").click();
+}
+
+if(getStore("filtre") == "rang") {
+  $("#filtreRang").click();
+} else {
+  $("#filtrePoints").click();
+}
+
+if(getStore("methode") == "flat") {
+  $("#filtreFlat").click();
+} else {
+  $("#filtreLessya").click();
+}
